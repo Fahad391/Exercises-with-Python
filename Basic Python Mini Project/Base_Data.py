22cea9d903265add947f8a00caf452492655ef8a
Md Farhan Rahman (Fahad) <mdfahadr12@gmail.com>
2026-02-28 20:46:58 +0600

    Add files via upload
    Just a small exercise with python, nothing serious

---
+++ Basic Python Mini Project/Base_Data.py
@@ -0,0 +1,20 @@
+room = 14
+main_gate_open = False
+food_supply = [
+    {f"Grain Quantity: {4000} Kilo"},
+    {f"Beef Quantity: {6500} Kilo"},
+    {f"Potato Quantity: {8957} Kilo"},
+    {f"Daal Quantity: {5000} Kilo"},
+]
+light_on = False
+
+resident_info = [
+    {"Name: ": "Sadat Ali", "Age: ": 35, "Occupation: ": "Doctor"},
+    {"Name: ": "Asiah Khan", "Age: ": 32, "Occupation: ": "Doctor"},
+    {"Name: ": "Abdul Malik", "Age: ": 28, "Occupation: ": "Mechanic"},
+    {"Name: ": "Fatima Noor", "Age: ": 30, "Occupation: ": "Biologist"},
+    {"Name: ": "Zainab Begum", "Age: ": 27, "Occupation: ": "Data Scientist"},
+    {"Name: ": "Hasnain Reza", "Age: ": 26, "Occupation: ": "Mechanic"},
+    {"Name: ": "Ayesha Siddiqui", "Age: ": 29, "Occupation: ": "Chemist"},
+    {"Name: ": "Omar Farooq", "Age: ": 31, "Occupation: ": "Data Analyst"}
+]
